c6a86dbe91a7ad62a8f7d24f9836da52b85d2e8b
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2024-05-06 06:09:00 +0700

    add count_word_occurrences

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -5,6 +5,5 @@ def get_device_id(environ):
     return environ.get("HTTP_DEVICE_ID", None)
 
 
-def count_word_occurrences(input_string, word):
-    count = sum(1 for _ in re.finditer(r"\b%s\b" % re.escape(word), input_string))
-    return count
+def count_word_occurrences(input_string: str, word: str) -> int:
+    return sum(1 for _ in re.finditer(r"\b%s\b" % re.escape(word), input_string))
